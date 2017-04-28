8707cce510b33ace04ef42859a474840977cc006
lauuteeh <lautaro.aragones@live.com.ar>
2017-04-28 18:19:47 -0300

    Controlador.Administracion | Creacion vista+accion

--- TrabajoPracticoWeb3/Controllers/AdministracionController.cs
+++ TrabajoPracticoWeb3/Controllers/AdministracionController.cs
@@ -13,5 +13,49 @@ namespace TrabajoPracticoWeb3.Controllers
         {
             return View();
         }
+
+        public ActionResult Peliculas()
+        {
+            return View();
+        }
+
+        //Accion para redireccionar a la vista para agragar una nueva pelicula
+        public ActionResult AltaPelicula()
+        {
+            return View();
+        }
+
+        public ActionResult Sedes()
+        {
+            return View();
+        }
+
+        //Accion para redireccionar a la vista para agragar una nueva sede
+        public ActionResult AltaSede()
+        {
+            return View();
+        }
+
+        public ActionResult Carteleras()
+        {
+            return View();
+        }
+
+        //Accion para redireccionar a la vista para agragar una nueva cartelera
+        public ActionResult AltaCartelera()
+        {
+            return View();
+        }
+
+        public ActionResult Reportes()
+        {
+            return View();
+        }
+
+        public ActionResult ListaReportes()
+        {
+            return View();
+        }
+
     }
 }
