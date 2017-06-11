35116418f5e57a90a3fa4cf756fc0d11aa59ff9f
lauuteeh <lauuteeh@users.noreply.github.com>
2017-06-11 00:49:42 -0300

    Model | Creacion Home Servicios

---
+++ TrabajoPracticoWeb3/Models/HomeServicios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrabajoPracticoWeb3.App_Data;
+
+namespace TrabajoPracticoWeb3.Models
+{
+    public class HomeServicios
+    {
+
+        public static dynamic CarteleraActual() {
+
+            myContext ctx = new myContext();
+            DateTime todaysDate = DateTime.Now;
+
+            var a = (from cartelera in ctx.Carteleras where cartelera.FechaFin >= todaysDate && cartelera.FechaInicio < todaysDate select cartelera).ToList(); ;
+
+            return a;
+
+        }
+    }
+}
