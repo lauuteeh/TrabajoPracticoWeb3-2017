86a85d1d813dbf8e1da88e9099e79b405b0e200d
ManuelTorresBaquedano <h.torres.baquedano@gmail.com>
2017-06-25 21:14:13 -0300

    Intento de Validacion Fecha
    Me deja avanzar sin necesidad de cargar fechas

--- TrabajoPracticoWeb3/Models/Reporte.cs
+++ TrabajoPracticoWeb3/Models/Reporte.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using TrabajoPracticoWeb3.App_Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace TrabajoPracticoWeb3.Models
 {
     public class Reporte
     {
+
+        
+        [Required(ErrorMessage = "Se requiere Fecha")]
         public DateTime FechaInicio { get; set; }
+        [Required(ErrorMessage = "Se requiere Fecha")]
         public DateTime FechaFin { get; set; }
 
     }
