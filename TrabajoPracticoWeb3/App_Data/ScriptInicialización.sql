a6b58f60287dd5c30d6478a8075dfd890d8eba45
julian <julianbermolen@gmail.com>
2017-06-26 15:28:10 -0300

    Script de inicialización en APPDATA

---
+++ TrabajoPracticoWeb3/App_Data/ScriptInicialización.sql
@@ -0,0 +1,18 @@
+use [20171C_TP];
+Insert into Calificaciones(Nombre) values ('ATP');
+Insert into Calificaciones(Nombre) values ('+13');
+Insert into Calificaciones(Nombre) values ('+13 con Reserva');
+Insert into Calificaciones(Nombre) values ('+16');
+Insert into Calificaciones(Nombre) values ('+16 con Reserva');
+
+Insert into Generos(Nombre) values ('Terror');
+Insert into Generos(Nombre) values ('Thriler');
+Insert into Generos(Nombre) values ('Accion');
+Insert into Generos(Nombre) values ('Comedia');
+Insert into Generos(Nombre) values ('Comedia romantica');
+
+Insert into Usuarios(NombreUsuario,Password) values ('admin','123');
+
+Insert into TiposDocumentos(Descripcion) values ('DNI');
+
+
